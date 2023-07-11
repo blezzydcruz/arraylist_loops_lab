2718dd51a993ced7cc02f47d8fb3c2888edac058
blezzydcruz <mbannedelacruz@hotmail.com>
2023-07-11 10:39:37 +0100

    updated with corrections

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -9,56 +8,52 @@ public class Runner {
 
 //        SCOTTISH ISLANDS
         List<String> scottishIslands = new ArrayList<>();
-        scottishIslands.add("Tiree");
         scottishIslands.add("Jura");
-        scottishIslands.add("Islay");
         scottishIslands.add("Mull");
         scottishIslands.add("Skye");
         scottishIslands.add("Aaran");
         scottishIslands.add("Tresco");
-        scottishIslands.add("Coll");
 
 //        1. Add "Coll" to the end of the list
-//        2. Add "Tiree" to the start of the list
-//        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add("Coll");
 
+
+//        2. Add "Tiree" to the start of the list
+        scottishIslands.add(0, "Tiree");
+
+
+//        3. Add "Islay" after "Jura" and before "Mull"
+        scottishIslands.add(2, "Islay");
         System.out.println("Scottish Islands: " +scottishIslands);
 
-//        4. Print out the index position of "Skye"
 
+//        4. Print out the index position of "Skye"
         System.out.println("Index position of Skye: " + scottishIslands.indexOf("Skye"));
 
+
 //        5. Remove "Tresco" from the list by name
-
         scottishIslands.remove("Tresco");
-
         System.out.println("Scottish Islands excluding Tresco: " + scottishIslands);
 
+
 //        6. Remove "Arran" from the list by index
-
-        int indexRemove = scottishIslands.indexOf("Aaran");
-        scottishIslands.remove(indexRemove);
-
+        scottishIslands.remove(5);
         System.out.println("Scottish Islands excluding Tresco & Aaran: " +scottishIslands);
 
 
 //        7. Print the number of islands in your arraylist
-
         System.out.println("Number of Island: " +scottishIslands.size());
 
-//        8. Sort the list alphabetically
 
+//        8. Sort the list alphabetically
         Collections.sort(scottishIslands);
 
-        System.out.println("Alphabetically Sorted Scottish Islands: " +scottishIslands);
 
 //        9. Print out all the islands using a for loop
+        for(String islands: scottishIslands) {
+            System.out.println(islands);
+        }
 
-        ArrayList <String> filteredScottishIslands = new ArrayList<>();
-
-        for(String islands: scottishIslands)
-
-        System.out.println(scottishIslands);
 
 //        NUMBERS
         List<Integer> numbers = new ArrayList<>();
@@ -77,8 +72,8 @@ public class Runner {
 
         System.out.println("Even numbers: " + evenNumbers);
 
-//        2. Print the difference between the largest and smallest value
 
+//        2. Print the difference between the largest and smallest value
         int min = Collections.min(numbers);
         int max = Collections.max(numbers);
 
@@ -86,8 +81,8 @@ public class Runner {
 
         System.out.println("Range: " + difference);
 
-//        3. Print True if the list contains a 1 next to a 1 somewhere.
 
+//        3. Print True if the list contains a 1 next to a 1 somewhere.
         boolean consecutiveOnes = false;
         for(int i = 0; i < numbers.size() -1; i++) {
             if(numbers.get(i) == 1 && numbers.get(i + 1) == 1) {
@@ -98,28 +93,26 @@ public class Runner {
 
         System.out.println("Consecutive ones?: " + consecutiveOnes);
 
-//        4. Print the sum of the numbers,
 
+//        4. Print the sum of the numbers,
         int sum = 0;
         for(int num : numbers) {
             sum += num;
         }
 
         System.out.println("Sum of numbers: " + sum);
+        
 
 //        5. Print the sum of the numbers...
 //           ...except the number 13 is unlucky, so it does not count...
 //           ...and numbers that come immediately after a 13 also do not count.
 //
 //          So [2, 7, 13, 2] would have sum of 9.
-
         int total = 0;
-
         for(int number : numbers) {
             if(number == 13) {
                 break;
             }
-
                 total += number;
             }
 
